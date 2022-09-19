4699bb6b8f31c7cb7f98f3620544f4443dc1ec95
Tudor Bizdoaca <tudor.bizdoaca@totalsoft.ro>
2022-09-19 14:15:35 +0300

    2:15

--- src/constants/menuConfig.js
+++ src/constants/menuConfig.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Dashboard } from '@material-ui/icons'
+import BallotIcon from '@material-ui/icons/Ballot';
 
-const menuItems = [{ icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' }]
+const menuItems = [{ icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },{icon: <BallotIcon />, text: 'NavBar.Vacations', path: '/vacations', name: 'Vacations'}]
 
 export default menuItems
