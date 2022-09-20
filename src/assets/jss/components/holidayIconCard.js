f167c295921fbe6c667b72bb8dcba02966655665
Madalina Mireag <madalina.mireag@totalsoft.ro>
2022-09-20 10:43:32 +0300

    cards istoric concedii

---
+++ src/assets/jss/components/holidayIconCard.js
@@ -0,0 +1,8 @@
+const holidayIcon = theme => {
+  return {
+    card: {
+      background: 'rgb(200, 160, 111)'
+    }
+  }
+}
+export default holidayIcon
--- src/features/istoric/IstoricConcedii.js
+++ src/features/istoric/IstoricConcedii.js
@@ -1,28 +1,43 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import ListIcon from '@material-ui/icons/List'
 import { IconCard } from '@bit/totalsoft_oss.react-mui.kit.core'
 import HolidayGrid from 'components/holiday/HolidayGrid'
-
+import { useTranslation } from 'react-i18next'
+import { makeStyles } from '@material-ui/core'
+import holidayIcon from 'assets/jss/components/holidayIconCard'
 function IstoricConcedii() {
+  const { t } = useTranslation()
+  const useStyles = makeStyles(holidayIcon)
+  const classes = useStyles()
   const holidays = [
     {
-      Id:0,
+      Id: 0,
       Perioada: '2022-08-12 - 2022-08-16',
       Inlocuitor: 'Teodorescu Mircea',
       Tip: 'odihna',
-      Status: 'Aprobat'
+      Status: 'Aprobat',
+      MotivRespingere: ''
     },
     {
-      Id:1,
+      Id: 1,
       Perioada: '2022-08-02 - 2022-08-06',
       Inlocuitor: 'Teodorescu Mircea',
       Tip: 'medical',
-      Status: 'Aprobat'
+      Status: 'Respins',
+      MotivRespingere: 'N-ai voie'
     }
   ]
-  console.log("TIP::::" + typeof(HolidayGrid))
+  console.log('TIP::::' + typeof HolidayGrid)
   console.log(holidays)
-  return (<IconCard icon={ListIcon} iconColor={'purple'} title={'Concedii'} content={<HolidayGrid holidays={holidays}/>} />)
+  return (
+    <IconCard
+      customCardClass={classes.card}
+      icon={ListIcon}
+      iconColor={'purple'}
+      title={t('Tooltips.Holidays')}
+      content={<HolidayGrid holidays={holidays} />}
+    />
+  )
 }
 
 export default IstoricConcedii
