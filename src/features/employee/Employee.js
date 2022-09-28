46ff4c6bd0094c85b301d7f818c7bc086b598c02
Alberto Chirita <alberto.chirita@totalsoft.ro>
2022-09-28 17:50:09 +0300

    started employee page

--- src/features/employee/Employee.js
+++ src/features/employee/Employee.js
@@ -1,14 +1,48 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import employeeStyle from 'features/employee/styles/employeeStyle'
-import dataEmployee from 'features/employee/data/dataEmployee'
+import { useHistory } from 'react-router-dom'
+import EmployeeHeader from './EmployeeHeader'
+import EmployeeMiniCard from './EmployeeMiniCard'
+import { Grid, TextField, Typography } from '@material-ui/core'
+import iconPhone from 'assets/img/iconPhone.svg'
+import iconEmail from 'assets/img/iconEmail.svg'
 
 const useStyles = makeStyles(employeeStyle)
 
 function Employee() {
   const classes = useStyles()
+  const history = useHistory()
 
-  return <div className={classes.page}>Employee</div>
+  const handleClick = () => {
+    history.push({ pathname: '/employees' })
+  }
+
+  return (
+    <div className={classes.page}>
+      <EmployeeHeader handleClick={handleClick} />
+      <Grid container spacing={4} direction='row' justifyContent='flex-end' alignItems='center' className={classes.contactInfo}>
+        <Grid item md={4} xs={12}>
+          <EmployeeMiniCard />
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <div className={classes.contactItem}>
+            <img className={classes.contactIcon} alt='iconPhone' src={iconEmail} />
+            <div>
+              <Typography className={classes.label}>Email</Typography>
+              <TextField variant='outlined' className={classes.textfield}></TextField>
+            </div>
+          </div>
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <div className={classes.contactItem}>
+            <img className={classes.contactIcon} alt='iconPhone' src={iconPhone} />
+            <span className={classes.contactText}>0727123456</span>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default Employee
--- src/features/employee/styles/employeeStyle.js
+++ src/features/employee/styles/employeeStyle.js
@@ -1,12 +1,27 @@
-import styles from 'assets/jss/styles'
-
-const employeeStyle = theme => {
+const employeeStyle = () => {
   return {
     page: {
       padding: '40px',
       backgroundColor: '#ffffff',
       boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.04)',
       borderRadius: '14px'
+    },
+    contactInfo: {
+      marginTop: '14px'
+    },
+    contactItem: {
+      display: 'flex',
+      textAlign: 'center'
+    },
+    contactIcon: {
+      width: '64px',
+      height: '64px'
+    },
+    contactText: {
+      marginLeft: '12px',
+      fontWeight: '600',
+      fontSize: '18px',
+      lineHeight: '19px'
     }
   }
 }
